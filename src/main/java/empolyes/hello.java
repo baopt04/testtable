878f11a9bd8aeaee8485fc2ffcd3dc3305b557d2
Bao Pham <phamthaibao2410@gmail.com>
2024-03-27 00:03:49 +0700

    add class hello

---
+++ src/main/java/empolyes/hello.java
@@ -0,0 +1,4 @@
+package empolyes;
+
+public class hello {
+}
---
+++ src/main/java/empolyes/hellogit.java
@@ -0,0 +1,7 @@
+package empolyes;
+
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
